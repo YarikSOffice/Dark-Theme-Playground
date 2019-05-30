e0d460b092c8c354d3e947fe7c5cce5cef02be88
YarikSOffice <yarikfreerun@gmail.com>
2019-05-30 15:05:21 +0300

    Make sure back button is available for all screens

--- app/src/main/java/example/com/darkthemeplayground/IgnoreOverrideForceDarkOptionActivity.kt
+++ app/src/main/java/example/com/darkthemeplayground/IgnoreOverrideForceDarkOptionActivity.kt
@@ -8,5 +8,11 @@ class IgnoreOverrideForceDarkOptionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ignore_override_force_dark_option)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
     }
 }
